61fa3a3198b4063c44fbee122d8ced912172deeb
smith46j <smith46j@cmich.edu>
2013-12-02 21:28:31 -0500

    Changed Position back to ManyToManyField

--- schedule/models.py
+++ schedule/models.py
@@ -9,7 +9,7 @@ class Employee(models.Model):
     min_hours = models.IntegerField()
     max_hours = models.IntegerField()
     target_hours = models.IntegerField()
-    position = models.ForeignKey('Position')
+    position = models.ManyToManyField('Position')
 
     def __unicode__(self):
         return u'{first} {last}'.format(first=self.first_name, last=self.last_name)
